b11af2e86d0e02f4c6c79c18711051f162cae32f
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-11 15:53:08 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+  # get 'posts', to: 'posts#index'
+  # get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
 end
