c57d954aea0f59dc851137807d9005800ea1b1e4
yuta-satou <68938732+yuta-satou@users.noreply.github.com>
2020-08-12 19:24:04 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content],checked: false)
+    # redirect_to action: :index
+    render json:{ post: post }
   end
 
   
